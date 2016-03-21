744f02e72b540bd86a9b3b4b17d937ddf58e1d99
Inthrayuth Mahaphol and Luca Quatela <luca@>
2016-03-21 11:21:26 -0700

    add the function to calculate the BMI

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,24 @@
 var add = function(number1, number2){
 	return number1 + number2;
-}
+};
 
 var subtract = function(number1, number2){
 	return number1 - number2;
-}
+};
 
 var multiply = function(number1, number2){
 	return number1 * number2;
-}
+};
 
 var divide = function(number1, number2){
 	return number1 / number2;
-}
+};
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
+var BMIcalculator = function(number1, number2){
+  return (number1) / (number2 * number2);
+};
+
+var number1 = parseFloat(prompt("Enter your Weight:"));
+var number2 = parseFloat(prompt("Enter your Height:"));
+var result = BMIcalculator(number1, number2);
 alert(result);
