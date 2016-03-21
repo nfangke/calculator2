c17148b38a2bcaf4750e8f59c1b405dc02609459
Inthrayuth Mahaphol and Luca Quatela <inthrayuth@>
2016-03-21 11:28:45 -0700

    add the function convert from fahrenheit to celsius

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,11 @@ var BMIcalculator = function(number1, number2){
   return (number1) / (number2 * number2);
 };
 
-var number1 = parseFloat(prompt("Enter your Weight:"));
+var fahrenheitToCelsius = function(number1){
+  return (number1 - 32) * (5/9);
+}
+
+var number1 = parseFloat(prompt("Enter your Celsius:"));
 var number2 = parseFloat(prompt("Enter your Height:"));
-var result = BMIcalculator(number1, number2);
+var result = fahrenheitToCelsius(number1 , number2);
 alert(result);
