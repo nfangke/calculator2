97134e63e791cdc7a0cd81a78f95ef45de9034c7
Luca Quatela and Nancy Fang <nancy@>
2016-03-23 15:27:44 -0700

    js with calculator

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,41 @@ var subtract = function(number1, number2){
 	return number1 - number2;
 };
 
+$(document).ready(function() {
+	$("form#add").submit(function(event) {
+		(event).preventDefault();
+		alert("before parseInt")
+		var number1 = parseInt($("input#add1").val());
+		var number2 = parseInt($("input#add2").val());
+    var result = add(number1 , number2);
+		alert("after addition")
+		$("#addoutput").text(result);
+
+	$("form#subtract").submit(function(event) {
+		(event).preventDefault();
+		var number1 = parseInt($("input#subtract1").val());
+		var number2 = parseInt($("input#subtract2").val());
+    var result = subtract(number1 , number2);
+
+		$("#subtractoutput").text(result);
+
+});
+});
+});
+
+/*
+$("form").submit(function() {
+parseFloat(prompt("Enter your Weight:"));
+var number2 = parseFloat(prompt("Enter your Height:"));
+*/
+
+
+
+/*
+var subtract = function(number1, number2){
+	return number1 - number2;
+};
+
 var multiply = function(number1, number2){
 	return number1 * number2;
 };
@@ -46,4 +81,4 @@ var kgToPound = function(number1) {
 var number1 = parseFloat(prompt("Enter your Weight:"));
 var number2 = parseFloat(prompt("Enter your Height:"));
 var result = kgToPound(number1 , number2);
-alert(result);
+alert(result);*/
